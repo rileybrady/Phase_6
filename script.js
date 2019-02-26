a2d7636ffb09f6962619fe6efc039d8fe32b0df9
rileybrady <37843519+rileybrady@users.noreply.github.com>
2019-02-26 13:47:07 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function nextImage(el){
+	if (el.src.match("images/distopia_1.jfif")){  
+		el.src = "images/green.jpg";
+		// if the image is a cherry, change it to a lemon!
+		}
+}
+		
+		function nextImage2(el){
+	if (el.src.match("images/distopia_2.jpg")){  
+		el.src = "images/green.jpg";
+		// if the image is a cherry, change it to a lemon!
+		}
+}
+
+		function nextImage3(el){
+	if (el.src.match("images/distopia_3.jfif")){  
+		el.src = "images/green.jpg";
+		// if the image is a cherry, change it to a lemon!
+		}
+}
